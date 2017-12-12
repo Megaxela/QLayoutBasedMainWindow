80297e16a71e7eb10ae89723baca53a80b085a19
Your Name <xelamegaxela@gmail.com>
2017-12-12 11:51:02 +0300

    Little ranged for loop fix

--- src/Windows/QLayoutMainWindow.cpp
+++ src/Windows/QLayoutMainWindow.cpp
@@ -39,7 +39,7 @@ void QLayoutMainWindow::updateAvailableLayouts()
         return;
     }
 
-    for (auto groupName : groups)
+    for (auto&& groupName : groups)
     {
         layouts.beginGroup(groupName);
 
